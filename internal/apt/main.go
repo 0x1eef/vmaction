efaf258734037c4e5a641a177f8619936f2ef2cc
0x1eef <0x1eef@protonmail.com>
2025-12-09 15:34:40 -0300

    Add apt implementation
    Inspired by https://raw.githubusercontent.com/vmactions/vbox/v1.1.8/vbox.sh

--- internal/apt/main.go
+++ internal/apt/main.go
@@ -1,5 +1,31 @@
 package apt
 
-const (
-	Foo = 1
+import (
+	"os/exec"
+
+	"github.com/hardenedbsd/hardenedbsd-vm/internal/cmd"
 )
+
+var (
+	packages = []string{
+		"zstd",
+		"libvirt-daemon-system",
+		"virt-manager",
+		"qemu-kvm",
+		"libosinfo-bin",
+		"sshpass",
+	}
+)
+
+func Run() error {
+	commands := [][]string{
+		{"sudo", "apt-get", "update"},
+		append([]string{"sudo", "apt-get", "install", "-y"}, packages...),
+	}
+	for _, c := range commands {
+		err := cmd.Run(exec.Command(c[0], c[1:]...))
+		if err != nil {
+			return err
+		}
+	}
+}
