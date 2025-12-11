6b7343c4074435b6a77d2b369504519964afbac5
0x1eef <0x1eef@protonmail.com>
2025-12-10 22:20:26 -0300

    Missing sudo, maybe?

--- socat.sh
+++ socat.sh
@@ -7,19 +7,13 @@ attempt=1
 
 ##
 # main
-sleep 10
-virsh list --all
-virsh domiflist testvm
-#virsh net-list --all
-#virsh net-dumpxml default
-
-#while [ $attempt -le $max ]; do
-#    IP=$(virsh domifaddr testvm | sed -n 's/.*ipv4 *\([0-9.]*\)\/.*/\1/p')
-#    if [ -n "$IP" ]; then
-#        echo -n "$IP"
-#        exit 0
-#    fi
-#    sleep 1
-#    attempt=$((attempt + 1))
-#done
+while [ $attempt -le $max ]; do
+    IP=$(sudo virsh domifaddr testvm | sed -n 's/.*ipv4 *\([0-9.]*\)\/.*/\1/p')
+    if [ -n "$IP" ]; then
+        echo -n "$IP"
+        exit 0
+    fi
+    sleep 1
+    attempt=$((attempt + 1))
+done
 exit 1
